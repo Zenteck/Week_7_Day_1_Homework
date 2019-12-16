38b34e85a67ac8636edef9a051fc493ede5e2d5c
Alan <spambox+github@zenteck.co.uk>
2019-12-16 18:52:37 +0000

    traced from today

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      tasks:[
+        {task: 'chew bone', done: false},
+        {task: 'chase cat', done: true},
+        {task: 'sleep', done: false},
+        {task: 'sit on laptop', done: true}
+      ],
+      newTask: ""
+    },
+    methods: {
+      saveNewTask: function(){
+        let taskObject = {task: this.newTask, done: false};
+        this.tasks.push(taskObject);
+        this.newTask = "";
+      },
+      completeTask: function(index){
+        this.tasks[index].done = true;
+      }
+    }
+
+  });
+});
