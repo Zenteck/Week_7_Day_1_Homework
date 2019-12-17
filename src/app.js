0d29eb4dec2d880e1dadcc0594c181f902b84586
Alan <spambox+github@zenteck.co.uk>
2019-12-17 08:08:53 +0000

    mvp

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue ({
-    em: "#app",
+    el: "#app",
     data: {
       tasks:[
-        {task: 'chew bone', done: false},
-        {task: 'chase cat', done: true},
-        {task: 'sleep', done: false},
-        {task: 'sit on laptop', done: true}
+        {task: 'chew bone', priority: 1, done: false},
+        {task: 'chase cat', priority: 2, done: true},
+        {task: 'sleep', priority: 1, done: false},
+        {task: 'sit on laptop', priority: 3, done: true}
       ],
       newTask: ""
     },
     methods: {
       saveNewTask: function(){
-        let taskObject = {task: this.newTask, done: false};
+        let taskObject = {task: this.newTask, priority: this.newTask, done: false};
         this.tasks.push(taskObject);
         this.newTask = "";
       },
